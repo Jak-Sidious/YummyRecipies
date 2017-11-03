7f01aaefc56437c13f51c098246dd9ac6edaa5f3
Jakana <k.jak86@yahoo.com>
2017-11-03 07:22:27 +0300

    Modify the user and recipie files
    Modify the user file to have a function to test if a login
    is succesful or not
    
    Modify the recipie file to cater to Adding, modifying and
    deleting recipies

--- app/recipie.py
+++ app/recipie.py
@@ -1,15 +1,43 @@
 '''This class deffines the recipie module'''
 class Recipie(object):
     '''The classs determining the recipies'''
-    def __init__(self, recipiename):
-        self.name = recipiename
-        self.ingridents = []
-        self.procdeure = []
+    def __init__(self):
+        self.recipies = {}
 
-    def add_ingrident(self, ingrid):
-        '''Method to add iningredients to a recipie'''
-        self.ingridents.append(ingrid)
+    def create_recipie(self, name, preparation=""" """):
+        '''this metthod allows for the creation of recipies'''
 
-    def add_procedure(self, proc):
-        '''Method to add procedure to recipie'''
-        self.procdeure.append(proc)
+        if name not in self.recipies.keys():
+            preparation = preparation.split("\r\n")
+            self.recipies[name] = preparation
+        else:
+            return "Recipie already exists"
+        return self.recipies
+
+    def browse(self, name):
+        '''
+        enables a user to view a food recipe
+        '''
+        if name in self.recipies.keys():
+            return self.recipies[name]
+        else:
+            return 'Not Available'
+
+    def modifyitems(self, name, new_name):
+        '''
+        changing the recipie items
+        '''
+        if name in self.recipies.keys():
+            self.recipies[new_name] = self.recipies[name]
+            del self.recipies[name]
+        else:
+            return self.recipies
+    
+    def delete(self, name):
+        '''method to delete items'''
+        if name in self.recipies.keys():
+            del self.recipies[name]
+        else:
+            return "Invalid Request"
+        return self.recipies
+
