d99e6a6a1fa3534b95ad890a6dd2735627147e3a
Jakana <k.jak86@yahoo.com>
2017-11-07 01:36:47 +0300

    Create the first and only test for recipie class

--- app/test/test_recipie.py
+++ app/test/test_recipie.py
@@ -2,15 +2,17 @@
 This module contains the test cases for the recipie class
 '''
 import unittest
-from app.models.recipie import Recipie
+from models.recipie import Recipe
 
 
 class Recipes(unittest.TestCase):
-    '''class determining all thetest cases for the recipie class'''
-    def testconstructor(self):
+    '''class determining all the test cases for the recipie class'''
+
+
+    def test_recipie_is_instance(self):
         '''Test case to ensure that the test constructor creates an
         object
         '''
-        result = Recipie()
+        result = Recipe('Recipie name', 'Your instructions here')
         self.assertIsInstance(result, object)
 
