522e9b6231e64dcd7ac964dd6583305e384fcbd6
Jakana <k.jak86@yahoo.com>
2017-11-07 03:49:13 +0300

    Create all the test cases and methods related to Category
    Test cases were designed for create, view, edit and delete
    Categories of food items

--- app/models/user.py
+++ app/models/user.py
@@ -1,16 +1,41 @@
 '''This module defines the user class'''
+
+from models.category import Category
+from models.recipie import Recipe
+
+
 class User(object):
     '''This class determines the user object to be used in the application'''
     def __init__(self, username, password):
         self.username = username
         self.password = password
+        self.categories = {}
 
-    #this should be one of the tests
-    def signin(self, uname, pword):
-        '''Method to determine if the username is correct or not'''
-        text1 = "You have succesfully entered your username and password"
-        text2 = "please enter the correct username and/or password"
-        if uname == self.username and pword == self.password:
-            return text1
-        else:
-            return text2
+    def categorycreate(self, name, description):
+        '''Creates the category and provides for the description for said
+           created category
+        '''
+        if description not in self.categories:
+            newest = Category(name, description)
+            self.categories[name] = newest
+            return self.categories
+        return "Why like dis!!!! This category exists already"
+
+    def view_categories(self, name):
+        '''Method to view categories'''
+        return self.categories[name]
+
+    def edit_category(self, category_name, description=None):
+        '''Edit a category'''
+        if description is None:
+            description = 'N/a'
+        update = Category(category_name, description)
+        self.categories[category_name] = update
+        return self.categories
+
+    def delete_category(self, category_name):
+        ''' Delete a particular category '''
+        del self.categories[category_name]
+        return self.categories
+
+
--- app/test/test_user.py
+++ app/test/test_user.py
@@ -1,16 +1,40 @@
 '''This module is used mainly to create test cases for the user class'''
 import unittest
-from app.models.user import User
+from models.user import User
 
 class Users(unittest.TestCase):
     '''Class conserning testcases to do with users'''
 
-    def test_user_constructor(self):
+    def setUp(self):
+        self.userr = User('fname', 'lname')
+        self.categoriz = self.userr.categories
+
+    def test_user_instance(self):
         '''Test case to ensure that the test constructor creates an
         object
         '''
-        result = User('fname', 'lname')
-        self.assertIsInstance(result, object)
+        self.assertIsInstance(self.userr, User)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_user_category_is_created(self):
+        '''Test if the category is created
+        '''
+        self.userr.categorycreate('name of category', 'description of category')
+        self.assertIn('name of category', self.userr.categories)
+
+    def test_category_is_viewable(self):
+        '''Test whether or not a category can be viewed'''
+        self.userr.categorycreate('name of category', 'description of category')
+        view = self.userr.view_categories('name of category')
+        self.assertEqual('name of category', view.category_name)
+
+    def test_if_category_is_editable(self):
+        '''Test whether or not a category can be edited'''
+        self.userr.categorycreate('name of category', 'description of category')
+        edit = self.userr.edit_category('name of category', 'descrip')
+        self.assertEqual('descrip', self.categoriz['name of category'].description)
+    
+    def test_if_category_is_deleted(self):
+        ''' test whether a category gets deleted'''
+        self.userr.categorycreate('name of category', 'description of category')
+        delete = self.userr.delete_category('name of category')
+        self.assertNotIn('name of category', self.categoriz)
